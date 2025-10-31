0fe2561838ebd38851066e524c48127d70519715
roxtardevil13 <61472798+roxtardevil13@users.noreply.github.com>
2025-10-31 10:55:56 +0530

    Delete entry.sh

--- entry.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Render provides $PORT; n8n expects N8N_PORT
-export N8N_PORT="${PORT:-5678}"
-# sensible default timezone; can be overridden by env
-export GENERIC_TIMEZONE="${GENERIC_TIMEZONE:-Asia/Kolkata}"
-exec /docker-entrypoint.sh start
